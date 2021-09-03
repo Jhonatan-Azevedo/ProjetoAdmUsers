d45f9bd2211655a859933e96fd0a195ef16e35dd
Jhonatan-Azevedo <dev.azevedo@outlook.com>
2021-09-03 15:21:12 -0300

    Painel de adm para usuarios

---
+++ index.js
@@ -0,0 +1,9 @@
+let = userController = new UserController("form-user-create", "form-user-update", "table-users");
+
+
+
+
+//MVC
+//Model = Controle de armazenamento de dados.
+//View = Interação com o usuario
+//Controller = ligando model e view.
